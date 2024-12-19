a91a654af002afe831ad5303d911566cbfda565f
erdiiinn <erdinshala01@gmail.com>
2024-12-19 07:10:06 -0800

    Changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,14 +1,51 @@
-console.log("Hello World");
+// console.log("Hello World");
 
-var a = 20;
-var b = 30;
-var D = a+b;
-var d = null;
-console.log(D);
+// var a = 20;
+// var b = 30;
+// var D = a+b;
+// var d = null;
+// console.log(D);
 
-var n = 99;
-n++
-console.log(n);
+// var n = 99;
+// n++
+// console.log(n);
 
-var a =50;
-console.log(a);
+// var a =50;
+// console.log(a);
+
+// var o =101;
+// o--;
+// console.log(o);
+
+// var x =25;
+// x +=5;
+// console.log(x);
+
+// var y = 50;
+// y /=3;
+// console.log(y);
+
+// var v = 5;
+// var w =5;
+// console.log(v==w);
+
+// var x = 60;
+// var y ="60";
+// console.log(x===y);
+
+
+// var firstName = "Erdin";
+// var lastName = "Shala";
+// var fullName = firstName + " " + lastName;
+// console.log(fullname);
+
+var age =16;
+console.log(age>16 && age <=65);
+
+var var 1 = 15;
+var var 2 = 60;
+console.log(var 1 = 15; || var 2 = 60);
+
+var var 3 = 9;
+var var 4 = 10;
+console.log(var 3 == var 4);
