ea0246987bf1eaf3c31b19e5b8fa8a8383f7e62b
erdiiinn <erdinshala01@gmail.com>
2024-12-24 07:07:42 -0800

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -39,13 +39,72 @@
 // var fullName = firstName + " " + lastName;
 // console.log(fullname);
 
-var age =16;
-console.log(age>16 && age <=65);
+// var age =16;
+// console.log(age>16 && age <=65);
 
-var var 1 = 15;
-var var 2 = 60;
-console.log(var 1 = 15; || var 2 = 60);
+// var var 1 = 15;
+// var var 2 = 60;
+// console.log(var 1 = 15; || var 2 = 60);
 
-var var 3 = 9;
-var var 4 = 10;
-console.log(var 3 == var 4);
+// var var 3 = 9;
+// var var 4 = 10;
+// console.log(var 3 == var 4);
+
+// function showMessage(){
+//     alert("This message is inside the function");
+// }
+// showMessage();
+
+// function sum(number1, number2){
+//     return number1 + number2;
+// }
+// console.log(sum(25, 5));
+
+// function toCelsius(f){
+//     return(5/9) * (f-32);
+// }
+
+// console.log("54 fahrenheit is equal to " +toCelsius(54)+" celsius");
+
+// function toMinutes(hours){
+//     return hours * 60;
+// }
+// console.log("1 hour is equal to " + toMinutes(1) + " minutes");
+
+// function triangleArea(baseLength, height){
+//     return 0.5 * baseLength * height;
+// }
+
+// console.log(triangleArea(5,7));
+
+// function dsFunction(){
+//     var localVar = "Digital School"
+//     alert(localVar);
+// }
+
+// dsFunction();
+
+// var arrowFunction = () => alert ("Hello World");
+
+// arrowFunction();
+
+// var arrowFunction = name => alert(`Hello ${name}`);
+// arrowFunction("John Doe");
+
+// var car = {
+//     name: "Mercedes",
+//     color: "red",
+//     year: 2020,
+//     kilometers;0,
+//     startEngine: function(){
+//         alert("The car is starting");
+//     },
+//     get getKilometers(){
+//         return this.kilometers;
+//     },
+
+//     set setKilometers(km){
+//         this.kilometers = km;
+//     }
+
+//     console.log(car.getKilometers);
