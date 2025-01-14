21fc260aeb871098bffbd3c129ae395893fd845f
erdiiinn <erdinshala01@gmail.com>
2025-01-14 07:13:45 -0800

    Changes

--- javascript/lesson28.js
+++ javascript/lesson28.js
@@ -8,23 +8,48 @@
 //     alert('hello world');
 // });
 
-var button2 = document.getElementById('btn1');
-var button3 = document.getElementById('btn2');
-var button4 = document.getElementById('btn3');
-var v_button = document.getElementById('button1');
-var v_txt = document.getElementById('txt1');
+// var button2 = document.getElementById('btn1');
 
-v_button.onclick = function(){
-    v_txt.style.color="red"
-    v_txt.style.padding="250px"
-}
+// var button3 = document.getElementById('btn2');
 
-button2.onmouseover= function(){
-    alert('over')
-}
-button3.onmouseleave= function(){
-    alert('leave')
-}
-button4.onmousewheel= function(){
-    alert('wheel')
+// var button4 = document.getElementById('btn3');
+
+// var v_button = document.getElementById('button1');
+
+// var v_txt = document.getElementById('txt1');
+
+// v_button.onclick = function(){
+//     // v_txt.style.color="red"
+//     // v_txt.style.backgroundColor="lightgrey"
+//     // v_txt.style.fontSize=""
+//     v_txt.setAttribute("class", "test")
+// }
+
+// v_txt.onmouseover = function(){
+//     v_txt.style = "color:red; background-color:blue; text-align:center; font-size:100px; padding:20px;"
+// }
+
+
+// button2.onmouseover= function(){
+//     alert('over')
+// }
+
+// button3.onmouseleave= function(){
+//     alert('leave')
+// }
+
+// button4.onmousewheel= function(){
+//     alert('wheel')
+// }
+
+var btn_circle=document.getElementById("btnCircle");
+var btn_circle=document.getElementById("btnRect");
+var btn_circle=document.getElementById("btnTriangle");
+
+var circle = document.getElementById("circle");
+var rect = document.getElementById("rect");
+var triangle = document.getElementById("triangle");
+
+btn_circle.onclick = function(){
+    
 }
