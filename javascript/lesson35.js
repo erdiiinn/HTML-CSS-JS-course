30bf24239a95cef8a1900544f286507ae2584e6c
erdiiinn <erdinshala01@gmail.com>
2025-02-13 07:13:53 -0800

    Lesson35

--- javascript/lesson35.js
+++ javascript/lesson35.js
@@ -39,3 +39,12 @@ function search(){
         }
     }
 }
+
+function openMenu(){
+    var menu = document.getElementById('myLinks');
+    if(menu.style.display === 'block'){
+        menu.style.display = 'none';
+    } else{
+        menu.style.display = 'block';
+    }
+}
